91a420ffa745c87011c2fcd0f1db511723e0e1f3
DankDuckz <87270285+DankDuckz@users.noreply.github.com>
2021-12-27 09:43:56 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+  apiKey: "AIzaSyDT2Qidp-Yq9rLb57BDOtwHPpRkRvwfWPU",
+  authDomain: "project71-a43f9.firebaseapp.com",
+  projectId: "project71-a43f9",
+  storageBucket: "project71-a43f9.appspot.com",
+  messagingSenderId: "983963170049",
+  appId: "1:983963170049:web:1e8863a13debff7ac15700"
+};
+
+  if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+
+export default firebase.firestore;
+
+
